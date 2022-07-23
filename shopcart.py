d10d7e68899f2dca131ccdfd50e443ecc8a1ad95
Justin <junitsu00@gmail.com>
2022-07-23 15:37:00 -0500

    first

---
+++ shopcart.py
@@ -0,0 +1,17 @@
+def from mmap import MADV_NOSYNC
+from operator import itemgetter
+
+
+receipt = [{item:price}]
+cart = ()
+#your choices
+
+
+
+#to add - input item > how many > how much > back to choices
+
+#to remove - input item > how many > back to choices
+
+#to view - print list/dict > back to choices
+
+#to checkout - print list
