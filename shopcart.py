4e4b7d7c30b1550bc4b7bea74458530d7b9bd1cd
Justin <junitsu00@gmail.com>
2022-07-24 12:16:20 -0500

    added random choice and text changes

--- shopcart.py
+++ shopcart.py
@@ -1,5 +1,14 @@
+import random
+
 cart = {}
 totalcost = 0
+
+
+posverbal = ["Perfect choice!", "You have excellent taste!", "How did you get so smart!"]
+posphrase = random.choice(posverbal)
+
+negverbal = ["They can't all be winners.","We understand. Everyone has a budget.","You should have known it was too many."]
+negphrase = random.choice(negverbal)
 
 def int_input(prompt):
     while True:
@@ -22,30 +31,33 @@ option = int(int_input("How can we help you today? "))
 
 while option != 4:   
     if option == 1:
-        item = input("Enter an item: ")
+        item = input("What item would you like to add? ")
         if item in cart:
-            print("Item is already in cart")
+            print("Item is already in cart.")
             qty = int(int_input("How many would you like to add? "))
             cart[item][0] += qty
-            print(f"{item} quantity is now {cart[item]}")
+            print(f"There are now {cart[item][0]} {item} in your cart.")
         else:
-            qty = int(int_input("Enter the quantity: "))
-            cost = float(input("How much does it cost? "))
+            print(posphrase)
+            qty = int(int_input("How many would you like to add? "))
+            cost = float(input("How much does one of those cost? "))
             cart[item] = [qty, cost]
     elif option == 2:
-        item = input("Enter an item: ")
+        item = input("Which item would you like to remove? ")
         if item in cart:
             qty = int(int_input("How many would you like to remove? "))
             cart[item][0] -= qty
             if cart[item][0] <= 0:
                 del(cart[item])
+                print((negphrase) + " This item has been removed from your cart.")
             else:
-                print(f"{item} quantity is now {cart[item]}")
+                print(f"There are now {cart[item][0]} {item} in your cart")
         else:
-            print("I'm sorry, this item is not in your cart.")
+            print("Apologies, this item is not in your cart.")
     elif option == 3:
         for item in cart:
             print(item, ":", cart[item][0], ":", cart[item][1])
+            print("That's a lot of quality products! Unless it's empty, then hurry up and buy.")
     elif option != 4: 
         print("Enter a valid number please.")
 
@@ -55,4 +67,4 @@ print("Shop smart. Shop S-Mart!")
 for item in cart:
     print(item, ":", cart[item][0], ":", cart[item][1])
     totalcost = cart[item][0] * cart[item][1] + totalcost
-print(totalcost)
+print("$ " + str(totalcost) + " total")
